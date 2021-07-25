36304f92cd38ad92d8871cc19fdad6aed7cbce10
wjl1123 <lee.woojae2@gmail.com>
2021-07-25 23:35:45 +0900

    deeplearn committing

--- DeepLearning/perceptron.py
+++ DeepLearning/perceptron.py
@@ -1,17 +1,22 @@
+import numpy as np
 
+x = np.array([0, 1])
+w = np.array([0.5, 0.5])
+b = -0.7
+w*x
+
+print(np.sum(w*x))
+print(np.sum(w*x)+b)
 
 def AND(x1, x2):
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp<= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <=0:
         return 0
-    elif tmp >theta:
+    else:
         return 1
 
-print(AND(0, 0))
-print(AND(1, 0))
-print(AND(0, 1))
-print(AND(1, 1))
-print(AND(2, 3))
 
-# version 1
+# version 2
